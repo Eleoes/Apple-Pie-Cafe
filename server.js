13a4df06068f87c9dadd710b8d01d6e122d063cf
eleoes <leo.escaroz@gmail.com>
2022-07-26 19:41:40 -0700

    app listening, mongoDB connected

---
+++ server.js
@@ -0,0 +1,43 @@
+/* =============== DEPENDENCIES =============== */
+const express = require('express');
+const mongoose = require('mongoose');
+const methodOverride = require('method-override');
+const morgan = require('morgan');
+const exp = require('constants');
+require('dotenv').config();
+
+// initialize app
+const app = express();
+
+/* =============== CONFIGURATION =============== */
+const PORT = process.env.PORT;
+const MONGO_URL = process.env.MONGO_URL;
+mongoose.connect(MONGO_URL);
+const db = mongoose.connection;
+
+/* All of this comes from Mongoose documentation */
+db.on('error', (error)=> {
+    console.error(error.message + `mongoDB error!`);
+})
+
+db.on('connected', () => {
+    console.log('mongoDB successfully connected');
+});
+
+db.on('disconnected', () => {
+    console.log('mongoDB disconnected');
+});
+
+/* =============== MIDDLEWARE =============== */
+app.use(express.urlencoded({extended: false})); // body-parser
+app.use(express.static('public'));
+app.use(methodOverride('_method'));
+app.use(morgan('dev'));
+
+/* =============== LISTENER =============== */
+app.listen(PORT, () => {
+    console.log(`Express is listening on ${PORT}`)
+});
+
+
+
